1d84b8faa4b2371834ffd75a07ffab90ffd53ec0
Sushant <sushantmane9594@gmail.com>
2022-12-03 01:18:03 +0530

    Book_My_Seat

--- Jdbc_01/src/com/practice_02/DBConnection.java
+++ Jdbc_01/src/com/practice_02/DBConnection.java
@@ -1,9 +1,24 @@
 package com.practice_02;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
 
-	public static void getConnection() {
+
 		
-	}
+		final static String url="jdbc:mysql://localhost:3306/wishtree";
+		final static String user="root";
+		final static String pass="Sushant@9930";
+		
+		public static Connection getConnection() throws ClassNotFoundException, SQLException 
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection DBConnection=DriverManager.getConnection(url,user,pass);
+			return DBConnection;
+		}
+
+	
 
 }
